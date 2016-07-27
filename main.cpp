60be0163c21a98bd3885bad1b1e784ee2e709bbe
musyoku <musyoku@users.noreply.github.com>
2016-07-28 00:57:07 +0900

    j

---
+++ main.cpp
@@ -0,0 +1,302 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <random>
+#include <chrono>
+#include <algorithm>
+#include <map>
+#include <unordered_map> 
+#include <boost/serialization/serialization.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/unordered_map.hpp>
+#include <stdio.h>
+#include <wchar.h>
+#include <locale>
+#include "node.h"
+#include "vpylm.h"
+#include "vocab.h"
+
+using namespace std;
+
+/*
+ * 動作確認
+ * OS X El Capitan
+ * gcc -v
+ * Apple LLVM version 7.0.0 (clang-700.0.72)
+ */
+
+Vocab* load(string &filename, vector<wstring> &dataset){
+	wifstream ifs(filename.c_str());
+	wstring str;
+	if (ifs.fail())
+	{
+		cout << "failed to load " << filename << endl;
+		return NULL;
+	}
+	Vocab* vocab = new Vocab();
+	while (getline(ifs, str))
+	{
+		dataset.push_back(str);
+		for(int i = 0;i < str.length();i++){
+			vocab->addCharacter(str[i]);
+		}
+	}
+	cout << "loading " << dataset.size() << " lines." << endl;
+	return vocab;
+}
+
+void show_progress(int step, int total, double &progress){
+	progress = step / (double)(total - 1);
+	int barWidth = 70;
+
+	cout << "[";
+	int pos = barWidth * progress;
+	for (int i = 0; i < barWidth; ++i) {
+		if (i < pos) cout << "=";
+		else if (i == pos) cout << ">";
+		else cout << " ";
+	}
+	cout << "] " << int(progress * 100.0) << " %\r";
+	cout.flush();
+
+	progress += 0.16; // for demonstration only
+}
+
+
+void vpylm_generate_sentence(Vocab* vocab, vector<wstring> &dataset, string model_dir){
+	HPYLM* hpylm = new HPYLM(3);
+	int num_chars = vocab->numCharacters();
+	hpylm->_g0 = 1.0 / (double)num_chars;
+
+	// 読み込み
+	hpylm->load(model_dir);
+	vocab->load(model_dir);
+
+	int num_sample = 100;
+	int max_length = 400;
+	vector<id> sentence_char_ids;
+	for(int s = 0;s < num_sample;s++){
+		sentence_char_ids.clear();
+		sentence_char_ids.push_back(vocab->bosId());
+		for(int i = 0;i < max_length;i++){
+			id word_id = hpylm->sampleNextWord(sentence_char_ids, vocab->eosId());
+			sentence_char_ids.push_back(word_id);
+			if(word_id == vocab->eosId()){
+				break;
+			}
+		}
+		wcout << vocab->characters2string(sentence_char_ids) << endl;
+	}
+}
+
+void train_vpylm(Vocab* vocab, vector<wstring> &dataset, string model_dir){
+	// 文脈木
+	VPYLM* vpylm = new VPYLM();
+	int num_chars = vocab->numCharacters();
+	vpylm->_g0 = 1.0 / (double)num_chars;
+	cout << "g0: " << vpylm->_g0 << endl;
+
+	vector<id> sentence_char_ids;
+
+	int max_epoch = 50;
+	// int train_per_epoch = dataset.size();
+	int train_per_epoch = dataset.size();
+
+	vector<int> rand_perm;
+	for(int i = 0;i < dataset.size();i++){
+		rand_perm.push_back(i);
+	}
+	random_shuffle(rand_perm.begin(), rand_perm.end());
+	// 以前のオーダー(VPYLM)
+	// [data_index][word_id][char_pos]
+
+	int** prev_orders = new int*[dataset.size()];
+	for(int data_index = 0;data_index < dataset.size();data_index++){
+		prev_orders[data_index] = NULL;
+	}
+
+	// 読み込み
+	vpylm->load(model_dir);
+	vocab->load(model_dir);
+
+	printf("training in progress...\n");
+	for(int epoch = 1;epoch < max_epoch;epoch++){
+		// cout << "##########################################" << endl;
+		// cout << "EPOCH " << epoch << endl;
+		// cout << "##########################################" << endl;
+		auto start_time = chrono::system_clock::now();
+		double progress = 0.0;
+		random_shuffle(rand_perm.begin(), rand_perm.end());
+
+		for(int step = 0;step < train_per_epoch;step++){
+
+			show_progress(step, train_per_epoch, progress);
+
+			int data_index = rand_perm[step];
+
+			wstring sentence = dataset[data_index];
+			if(sentence.length() == 0){
+				continue;
+			}
+			sentence_char_ids.clear();
+			sentence_char_ids.push_back(vocab->bosId());
+			for(int i = 0;i < sentence.length();i++){
+				int id = vocab->char2id(sentence[i]);
+				sentence_char_ids.push_back(id);
+			}
+			sentence_char_ids.push_back(vocab->eosId());
+
+			if(prev_orders[data_index] == NULL){
+				prev_orders[data_index] = new int[sentence_char_ids.size()];
+			}else{
+				for(int c_t_i = 0;c_t_i < sentence_char_ids.size();c_t_i++){
+					int n_t = prev_orders[data_index][c_t_i];
+					bool success = vpylm->remove(sentence_char_ids, c_t_i, n_t);
+					if(success == false){
+						printf("\x1b[41;97m");
+						printf("WARNING");
+						printf("\x1b[49;39m");
+						printf(" Failed to remove a customer from VPYLM.\n");
+					}
+				}				
+			}
+
+			for(int c_t_i = 0;c_t_i < sentence_char_ids.size();c_t_i++){
+				int n_t = vpylm->sampleOrder(sentence_char_ids, c_t_i);
+				vpylm->add(sentence_char_ids, c_t_i, n_t);
+				prev_orders[data_index][c_t_i] = n_t;
+			}
+		}
+
+		vpylm->sampleHyperParams();
+
+		auto end_time = chrono::system_clock::now();
+		auto dur = end_time - start_time;
+		auto msec = chrono::duration_cast<chrono::milliseconds>(dur).count();
+
+		// パープレキシティ
+		double vpylm_ppl = 0;
+		for(int i = 0;i < train_per_epoch;i++){
+			wstring sentence = dataset[rand_perm[i]];
+			if(sentence.length() == 0){
+				continue;
+			}
+			// if(sentence.length() > 200){
+			// 	continue;
+			// }
+			sentence_char_ids.clear();
+			sentence_char_ids.push_back(vocab->bosId());
+			for(int i = 0;i < sentence.length();i++){
+				int id = vocab->char2id(sentence[i]);
+				sentence_char_ids.push_back(id);
+			}
+			sentence_char_ids.push_back(vocab->eosId());
+			double log_p = vpylm->log_Pw(sentence_char_ids) / (double)sentence_char_ids.size();
+			vpylm_ppl += log_p;
+		}
+		vpylm_ppl = exp(-vpylm_ppl / (double)train_per_epoch);
+
+		cout << endl << "[epoch " << epoch << "] " <<  (double)train_per_epoch / msec * 1000.0 << " sentences / sec; perplexity " << vpylm_ppl << endl;
+
+		// if(epoch % 10 == 0){
+		// 	vpylm->save(model_dir);
+		// 	vocab->save(model_dir);
+		// }
+
+	}
+
+	// vpylm->save(model_dir);
+	// vocab->save(model_dir);
+
+
+	// 客を全て削除した時に客数が本当に0になるかを確認する場合
+	for(int step = 0;step < train_per_epoch;step++){
+		// cout << "\x1b[40;97m[STEP]\x1b[49;39m" << endl;
+		int data_index = rand_perm[step];
+
+		wstring sentence = dataset[data_index];
+		if(sentence.length() == 0){
+			continue;
+		}
+		sentence_char_ids.clear();
+		sentence_char_ids.push_back(vocab->bosId());
+		for(int i = 0;i < sentence.length();i++){
+			int id = vocab->char2id(sentence[i]);
+			sentence_char_ids.push_back(id);
+		}
+		sentence_char_ids.push_back(vocab->eosId());
+
+		for(int c_t_i = 0;c_t_i < sentence_char_ids.size();c_t_i++){
+			if(prev_orders[data_index] != NULL){
+				int n_t = prev_orders[data_index][c_t_i];
+				bool success = vpylm->remove(sentence_char_ids, c_t_i, n_t);
+				if(success == false){
+					printf("\x1b[41;97m");
+					printf("WARNING");
+					printf("\x1b[49;39m");
+					printf(" Failed to remove a customer from VPYLM.\n");
+				}
+			}
+		}
+	}
+	cout << vpylm->maxDepth() << endl;
+	cout << vpylm->numChildNodes() << endl;
+	cout << vpylm->numCustomers() << endl;
+
+
+	for(int data_index = 0;data_index < train_per_epoch;data_index++){
+		delete[] prev_orders[data_index];
+	}
+	delete[] prev_orders;
+}
+
+int main(int argc, char *argv[]){
+	// 日本語周り
+	setlocale(LC_CTYPE, "ja_JP.UTF-8");
+	ios_base::sync_with_stdio(false);
+	locale default_loc("ja_JP.UTF-8");
+	locale::global(default_loc);
+	locale ctype_default(locale::classic(), default_loc, locale::ctype); //※
+	wcout.imbue(ctype_default);
+	wcin.imbue(ctype_default);
+	vector<wstring> dataset;
+
+	// [arguments]
+	// --textdir ****
+	// 訓練データtrain.txtの入っているフォルダを指定
+
+	string filename;
+	string model_dir;
+	cout << "num args = " << argc << endl;
+	if(argc % 2 != 1){
+		cout << "invalid command line arguments." << endl;
+		return -1;
+	}else{
+		for (int i = 0; i < argc; i++) {
+			cout << i << "-th args = " << argv[i] << endl; 
+			if (string(argv[i]) == "--text_dir") {
+				if(i + 1 >= argc){
+					cout << "invalid command line arguments." << endl;
+					return -1;
+				}
+				filename = string(argv[i + 1]) + string("/train.txt");
+			}
+			else if (string(argv[i]) == "--model_dir") {
+				if(i + 1 >= argc){
+					cout << "invalid command line arguments." << endl;
+					return -1;
+				}
+				model_dir = string(argv[i + 1]) + string("/");
+			}
+		}
+	}
+
+	Vocab* vocab = load(filename, dataset);
+
+	train_vpylm(vocab, dataset, model_dir);
+	// hpylm_generate_sentence(vocab, dataset, model_dir);
+	return 0;
+}
---
+++ vpylm.h
@@ -0,0 +1,447 @@
+#ifndef _vpylm_
+#define _vpylm_
+#include <vector>
+#include <random>
+#include <cmath>
+#include <unordered_map> 
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/base_object.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/serialization/vector.hpp>
+#include "sampler.h"
+#include "hpylm.h"
+#include "node.h"
+
+class VPYLM: public HPYLM{
+private:
+	friend class boost::serialization::access;
+	template <class Archive>
+	void serialize(Archive& archive, unsigned int version)
+	{
+		static_cast<void>(version); // No use
+		archive & _root;
+		archive & _g0;
+		archive & _beta_stop;
+		archive & _beta_pass;
+		archive & _d_m;
+		archive & _theta_m;
+		archive & _a_m;
+		archive & _b_m;
+		archive & _alpha_m;
+		archive & _beta_m;
+	}
+public:
+	double _beta_stop;		// 停止確率q_iのベータ分布の初期パラメータ
+	double _beta_pass;		// 停止確率q_iのベータ分布の初期パラメータ
+
+	VPYLM(){
+		_root = new Node(0);
+		_root->_depth = 0;	// ルートは深さ0
+
+		// http://www.ism.ac.jp/~daichi/paper/ipsj07vpylm.pdfによると初期値は(4, 1)
+		// しかしVPYLMは初期値にあまり依存しないらしい
+		_beta_stop = 4;
+		_beta_pass = 1;
+	}
+	// n_tはw_tから見た深さ
+	void add(vector<id> &context, int w_t_i, int n_t){
+		if(n_t > w_t_i){
+			// cout << "VPYLM::add invalid order." << endl;
+			return;
+		}
+		int w_t = context[w_t_i];
+
+		// cout << "VPYLM::add called with context: ";
+  //       for(int i = 0;i < context.size();i++){
+  //           cout << context[i] << ",";
+  //       }
+		// cout <<" w_t: " << w_t << " n_t: " << n_t << endl;
+
+		Node* node = _root;
+		for(int depth = 0;depth < n_t;depth++){
+			int u_t = context[w_t_i - depth - 1];
+			Node* child = node->generateChildIfNeeded(u_t);
+			if(child == NULL){
+				// cout << "internal error occurred." << endl;
+				return;
+			}
+			node = child;
+		}
+		double parent_p_w = _g0;
+		if(node->_parent){
+			parent_p_w = node->_parent->Pw(w_t, _g0, _d_m, _theta_m);
+		}
+		node->addCustomer(w_t, parent_p_w, _d_m, _theta_m);
+	}
+
+	bool remove(vector<id> &context, int w_t_i, int n_t){
+		int w_t = context[w_t_i];
+		Node* node = _root;
+		for(int depth = 0;depth < n_t;depth++){
+			int u_t = context[w_t_i - depth - 1];
+			Node* child = node->findChildWithId(u_t);
+			if(child == NULL){
+				return false;
+			}
+			node = child;
+		}
+
+		bool should_remove_from_parent = false;
+		node->removeCustomer(w_t, should_remove_from_parent);
+		if(should_remove_from_parent && node->_parent != NULL){
+			node->_parent->deleteChildWithId(node->_id);
+		}
+		return true;
+	}
+
+	int sampleOrder(vector<id> &context_ids, int w_t_i){
+		// cout << "VPYLM::sampleOrder called with context_ids: ";
+  //       for(int i = 0;i < context_ids.size();i++){
+  //           cout << context_ids[i] << ",";
+  //       }
+		// cout << endl;
+		if(w_t_i == 0){
+			return 0;
+		}
+		// int max_n = w_t_i;
+		// if(max_order != -1){
+		// 	max_n = w_t_i > max_order ? max_order : w_t_i;
+		// }
+		int w_t = context_ids[w_t_i];
+		// cout << "w_t: " << w_t << " w_t_i: " << w_t_i << endl;
+		// cout << "_g0: " << _g0 << endl;
+		vector<double> probs;
+		double sum_p_stpp = 0;
+		// この値を下回れば打ち切り
+		double eps = 1e-6;
+		
+		double rand_max = 0;
+		double p_pass = 0;
+		double Pw = 0;
+		Node* node = _root;
+		for(int n = 0;n <= w_t_i;n++){
+			// cout << "n = " << n << endl;
+			// cout << "w_t: " << w_t << endl;
+			if(node){
+				// cout << "node: exists. depth: " << node->_depth << " word_id: " << node->_id << endl;
+				Pw = node->Pw(w_t, _g0, _d_m, _theta_m);
+				// cout << "Pw: " << Pw << endl;
+				double p_stop = node->p_stop(_beta_stop, _beta_pass);
+				// cout << "p_stop: " << p_stop << endl;
+				p_pass = node->p_pass(_beta_stop, _beta_pass);
+				double p = Pw * p_stop;
+				// cout << "pw:  " << Pw << ", p_stop: " << p_stop << endl;
+				probs.push_back(p);
+				sum_p_stpp += p_stop;
+				rand_max += probs[n];
+
+				if(p < eps){
+					break;
+				}
+				if(n < w_t_i){
+					int u_t = context_ids[w_t_i - n - 1];
+					// cout << "u_t: " << u_t << endl;
+					node = node->findChildWithId(u_t);
+				}
+			}else{
+				// cout << "node: none" << endl;
+				// cout << "Pw: " << Pw << endl;
+				double p_stop = p_pass * _beta_stop / (_beta_stop + _beta_pass);
+				// cout << "p_stop: " << p_stop << endl;
+				double p = Pw * p_stop;
+				// cout << "p: " << p << endl;
+				probs.push_back(p);
+				sum_p_stpp += p_stop;
+				rand_max += probs[n];
+				p_pass *= _beta_pass / (_beta_stop + _beta_pass);
+				// cout << "p_pass: " << p_pass << endl;
+
+				if(p < eps){
+					break;
+				}
+			}
+		}
+
+		// cout << "sum_p_stpp: " << sum_p_stpp << endl;
+		// cout << "rand_max: " << rand_max << endl;
+		// cout << "probs" << endl;
+		// for(int n = 0;n <= max_n;n++){
+		// 	cout << probs[n] << endl;
+		// }
+		double ratio = 1.0 / rand_max;
+		uniform_real_distribution<double> rand(0, 1);
+		double r = rand(Sampler::mt);
+		// cout << "rand: " << r << endl;
+		double sum = 0;
+		for(int n = 0;n < probs.size();n++){
+			sum += probs[n] * ratio;
+			if(r < sum){
+				// cout << "return " << n << endl;
+				return n;
+			}
+		}
+		// cout << "return " << probs.size() << endl;
+		return probs.size();
+	}
+
+	// contextからwordが生成された確率
+	// word = {うさぎですか}	context_ids = {ご注文は}
+	// Pw_h(word, context_ids) = P(うさぎですか|ご注文は)
+	double Pw_h(vector<id> &word_ids, vector<id> context_ids, bool fixed_depth = false){
+		// cout << "p_w_h2 called with context_ids: ";
+		// for(int i = 0;i < context_ids.size();i++){
+			// cout << context_ids[i] << ",";
+		// }
+		// cout << " word_ids: ";
+		// for(int i = 0;i < word_ids.size();i++){
+			// cout << word_ids[i] << ",";
+		// }
+		// cout << endl;
+
+		double p = 1;
+		for(int n = 0;n < word_ids.size();n++){
+			p *= Pw_h(word_ids[n], context_ids, fixed_depth);
+			context_ids.push_back(word_ids[n]);
+		}
+		return p;
+	}
+
+	// P(w|h) = sum_n P(w|h,n)P(n|h)
+	double Pw_h(id word_id, vector<id> &context_ids, bool fixed_depth = false){
+		// cout << "Pw_h called with context_ids: ";
+  //       for(int i = 0;i < context_ids.size();i++){
+  //           cout << context_ids[i] << ",";
+  //       }
+  //       cout << " word_id: " << word_id << endl;
+
+		// どの深さまでノードが存在するかを調べる
+		Node* node = _root;
+		int depth = 0;
+		for(;depth < context_ids.size();depth++){
+			int u_t = context_ids[context_ids.size() - depth - 1];
+			// cout << "u_t: " << u_t << endl;
+			if(node == NULL){
+				break;
+			}
+			Node* child = node->findChildWithId(u_t);
+			if(child == NULL){
+				break;
+			}
+			node = child;
+		}
+		// cout << "depth: " << depth << endl;
+		if(fixed_depth && depth != context_ids.size()){
+			return 0;
+		}
+		double p = 0;
+		for(int n = 0;n <= depth;n++){
+			double a = Pw_hn(word_id, context_ids, n);
+			double b = Pn_h(n, context_ids);
+			// cout << "p += " << a << " * " << b << endl;
+			p += a * b;
+		}
+		// cout << "depth available" << endl;
+		return p;
+	}
+
+	// P(w|h,n)
+	// 可能な深さ全てのノードが存在している必要がある
+	double Pw_hn(id word_id, vector<id> &context_ids, int n){
+		// cout << "Pw_hn called with context_ids: ";
+  //       for(int i = 0;i < context_ids.size();i++){
+  //           cout << context_ids[i] << ",";
+  //       }
+  //       cout << " word_id: " << word_id << " n: " << n << endl;
+
+		if(n > context_ids.size()){
+			// ノードが無い場合は0を返す
+			printf("\x1b[41;97m");
+			printf("WARNING");
+			printf("\x1b[49;39m");
+			printf(" n > context_ids.size() at VPYLM::Pw_hn\n");
+			return 0;
+		}
+
+		// どの深さまでノードが存在するかを調べる
+		Node* node = _root;
+		int depth = 0;
+		for(;depth < n;depth++){
+			int u_t = context_ids[context_ids.size() - depth - 1];
+			// cout << "u_t: " << u_t << endl;
+			if(node == NULL){
+				break;
+			}
+			Node* child = node->findChildWithId(u_t);
+			if(child == NULL){
+				break;
+			}
+			node = child;
+		}
+		// cout << "depth: " << depth << endl;
+
+		if(depth != n){
+			// ノードが無い場合は0を返す
+			printf("\x1b[41;97m");
+			printf("WARNING");
+			printf("\x1b[49;39m");
+			printf(" depth != n at VPYLM::Pw_hn\n");
+			return 0;
+		}
+
+		double p = node->Pw(word_id, _g0, _d_m, _theta_m);
+		// cout << "return: " << p << endl;
+		return p;
+	}
+
+	// P(n|h)
+	// 可能な深さ全てのノードが存在している必要がある
+	double Pn_h(int n, vector<id> &context_ids){
+		// cout << "Pn_h called with context_ids: ";
+		// for(int i = 0;i < context_ids.size();i++){
+		//     cout << context_ids[i] << ",";
+		// }
+		// cout << " n: " << n << endl;
+
+		if(n > context_ids.size()){
+			// ノードが無い場合は0を返す
+			printf("\x1b[41;97m");
+			printf("WARNING");
+			printf("\x1b[49;39m");
+			printf(" n > context_ids.size() at VPYLM::Pw_h\n");
+			return 0;
+		}
+
+		Node* node = _root;
+		int depth = 0;
+		for(;depth < n;depth++){
+			int u_t = context_ids[context_ids.size() - depth - 1];
+			// cout << "u_t: " << u_t << endl;
+			if(node == NULL){
+				break;
+			}
+			Node* child = node->findChildWithId(u_t);
+			if(child == NULL){
+				break;
+			}
+			node = child;
+		}
+		// cout << depth << endl;
+		if(depth != n){
+			// ノードが無い場合は0を返す
+			printf("\x1b[41;97m");
+			printf("WARNING");
+			printf("\x1b[49;39m");
+			printf(" depth != n at VPYLM::Pn_h\n");
+			return 0;
+		}
+		// cout << *node << endl;
+
+		// 親ノードまで遡って計算される
+		return node->p_stop(_beta_stop, _beta_pass);
+	}
+
+	// P(word)
+	// s = abcならP(abc) = P(c|ab)P(b|a)P(a)
+	double Pw(vector<id> &word_ids){
+		if(word_ids.size() == 0){
+			return 0;
+		}
+		// cout << "VPYLM::Pw called with word_ids: ";
+  //       for(int i = 0;i < word_ids.size();i++){
+  //           cout << word_ids[i] << ",";
+  //       }
+  //       cout << endl;
+		int w_0 = word_ids[0];
+		// cout << "w_0: " << w_0 << endl;
+		double p0 = _root->Pw(w_0, _g0, _d_m, _theta_m) * _root->p_stop(_beta_stop, _beta_pass);
+		// cout << "p0: " << p0 << endl;
+		double p = p0;
+		vector<id> h(word_ids.begin(), word_ids.begin() + 1);
+		for(int depth = 1;depth < word_ids.size();depth++){
+			vector<id> w = {word_ids[depth]};
+			// cout << "w: " << word_ids[depth] << endl;
+			// cout << "h: ";
+			// for(int j = 0;j < h.size();j++){
+				// cout << h[j] << ",";
+			// }
+			// cout << endl;
+			double _p = Pw_h(w, h);
+			// cout << "p *= " << _p << endl;
+			p *= _p;
+			h.push_back(word_ids[depth]);
+		}
+		// cout << "return: " << p << endl;
+		return p;
+	}
+
+	double log_Pw(vector<id> &word_ids){
+		if(word_ids.size() == 0){
+			return 0;
+		}
+		// cout << "VPYLM::Pw called with word_ids: ";
+  //       for(int i = 0;i < word_ids.size();i++){
+  //           cout << word_ids[i] << ",";
+  //       }
+  //       cout << endl;
+		int w_0 = word_ids[0];
+		// cout << "w_0: " << w_0 << endl;
+		double p0 = _root->Pw(w_0, _g0, _d_m, _theta_m) * _root->p_stop(_beta_stop, _beta_pass);
+		// cout << "p0: " << p0 << endl;
+		double p = log2(p0 + 1e-10);
+		vector<id> h(word_ids.begin(), word_ids.begin() + 1);
+		for(int depth = 1;depth < word_ids.size();depth++){
+			vector<id> w = {word_ids[depth]};
+			// cout << "w: " << word_ids[depth] << endl;
+			// cout << "h: ";
+			// for(int j = 0;j < h.size();j++){
+				// cout << h[j] << ",";
+			// }
+			// cout << endl;
+			double _p = Pw_h(w, h);
+			// cout << "p *= " << _p << endl;
+			p += log2(_p + 1e-10);
+			h.push_back(word_ids[depth]);
+		}
+		// cout << "return: " << p << endl;
+		return p;
+	}
+
+	int maxDepth(){
+		return _d_m.size() - 1;
+	}
+
+	int numChildNodes(){
+		return _root->numChildNodes();
+	}
+
+	int numCustomers(){
+		return _root->numCustomers();
+	}
+
+	void save(string dir = "model/"){
+		string filename = dir + "vpylm.model";
+		std::ofstream ofs(filename);
+		boost::archive::binary_oarchive oarchive(ofs);
+		oarchive << static_cast<const VPYLM&>(*this);
+		cout << "saved to " << filename << endl;
+		cout << "	num_customers: " << numCustomers() << endl;
+		cout << "	num_nodes: " << numChildNodes() << endl;
+		cout << "	max_depth: " << maxDepth() << endl;
+	}
+
+	void load(string dir = "model/"){
+		string filename = dir + "vpylm.model";
+		std::ifstream ifs(filename);
+		if(ifs.good()){
+			cout << "loading " << filename << endl;
+			boost::archive::binary_iarchive iarchive(ifs);
+			iarchive >> *this;
+			cout << "	num_customers: " << numCustomers() << endl;
+			cout << "	num_nodes: " << numChildNodes() << endl;
+			cout << "	max_depth: " << maxDepth() << endl;
+		}
+	}
+};
+
+#endif
